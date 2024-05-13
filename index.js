f08f44a91270b70b31c2d9166541feac3fdb9a13
Asfahmussarrat <160323759+Asfahmussarrat@users.noreply.github.com>
2024-05-13 10:24:32 -0700

    project2 CLI-GUESSING GAME

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+// computer generate a random number
+//user willguess the number input by user
+//compair user number and computer random number
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    { name: "userGuessedNumber",
+        type: "number",
+        message: "Please Guess a number between:1-10 ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation!you are winner.");
+}
+else {
+    console.log("sorry!your guess get wrong.");
+}
